3f264c7081e89c592db14c9ce4988b44cd221565
jwinder <jsph.winder@gmail.com>
2015-02-21 11:37:00 -0600

    Cleaning

--- mine/mine-linux.el
+++
@@ -1,6 +0,0 @@
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome")
-(setq ack-executable "ack-grep")
-
-(provide 'mine-linux)
