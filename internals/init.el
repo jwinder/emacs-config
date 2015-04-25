e6e209a62c41ec198e022291957c19e97913f1a0
jwinder <jsph.winder@gmail.com>
2015-04-25 15:14:45 -0500

    Next release 2.5.1

--- internals/init.el
+++ internals/init.el
@@ -2,8 +2,7 @@
 (defconst jw-config-author-email "jsph.winder@gmail.com")
 (defconst jw-config-author-url "http://jwinder.io")
 (defconst jw-config-github-url "https://github.com/jwinder/emacs")
-(defconst jw-config-version "2.5.0")
-
+(defconst jw-config-version "2.5.1")
 (defconst adamdecaf-config-github-url "https://github.com/adamdecaf/emacs.d")
 (defconst knuckolls-config-github-url "https://github.com/knuckolls/knuckolls-emacs-config")
 (defconst rubbish-config-github-url "https://github.com/rubbish/rubbish-emacs-setup")
--- public/emacs.el
+++ public/emacs.el
@@ -21,41 +21,41 @@
   (mapconcat 'number-to-string components "."))
 
 (defun emacs-config--get-next-bugfix-version ()
-  (let ((ns (emacs--get-version-components)))
-    (emacs--make-version-number `(,(car ns) ,(nth 1 ns) ,(+ 1 (nth 2 ns))))))
+  (let ((ns (emacs-config--get-version-components)))
+    (emacs-config--make-version-number `(,(car ns) ,(nth 1 ns) ,(+ 1 (nth 2 ns))))))
 
 (defun emacs-config--get-next-minor-version ()
-  (let ((ns (emacs--get-version-components)))
-    (emacs--make-version-number `(,(car ns) ,(+ 1 (nth 1 ns)) 0))))
+  (let ((ns (emacs-config--get-version-components)))
+    (emacs-config--make-version-number `(,(car ns) ,(+ 1 (nth 1 ns)) 0))))
 
 (defun emacs-config--get-next-major-version ()
-  (let ((ns (emacs--get-version-components)))
-    (emacs--make-version-number `(,(+ 1 (car ns)) 0 0))))
+  (let ((ns (emacs-config--get-version-components)))
+    (emacs-config--make-version-number `(,(+ 1 (car ns)) 0 0))))
 
-(defun emacs-config--update-readme ()
-  (let* ((header "Emacs Config")
-         (content (replace-regexp-in-string ";; " "" (make-initial-hello-message)))
-         (file (concat user-emacs-directory "README.md"))
-         (readme (concat "## " header "\n\n" content)))
-    (write-region readme nil file)))
+(defun emacs-config--update-version-in-emacs-config (old-version next-version)
+  (let* ((filename (concat user-emacs-directory "internals/init.el"))
+         (contents (mapconcat 'identity (jw--read-file-lines-to-string filename) "\n"))
+         (updated-contents (replace-regexp-in-string old-version next-version contents)))
+    (write-region updated-contents nil filename)))
 
 (defun emacs-config--commit-tag-push (tag-version)
   (let ((git-command (format "cd %s && git add -A && git commit -m \"Next release %s\" && git tag %s && git push --tags origin master"
                              user-emacs-directory tag-version tag-version)))
     (shell-command git-command)))
 
-(defun emacs-config--release-version (next-version)
+(defun emacs-config--release-version (previous-version next-version)
   (setq jw-config-version next-version)
+  (emacs-config--update-version-in-emacs-config previous-version next-version)
   (emacs-config--commit-tag-push next-version))
 
 (defun emacs-config-bump-bugfix-version ()
   (interactive)
-  (emacs-config--release-version (emacs-config--get-next-bugfix-version)))
+  (emacs-config--release-version jw-config-version (emacs-config--get-next-bugfix-version)))
 
 (defun emacs-config-bump-minor-version ()
   (interactive)
-  (emacs-config--release-version (emacs-config--get-next-minor-version)))
+  (emacs-config--release-version jw-config-version (emacs-config--get-next-minor-version)))
 
 (defun emacs-config-bump-major-version ()
   (interactive)
-  (emacs-config--release-version (emacs-config--get-next-major-version)))
+  (emacs-config--release-version jw-config-version (emacs-config--get-next-major-version)))
