71f75b65a75243d503d670c840c97d6daf436d18
jwinder <jsph.winder@gmail.com>
2015-07-25 11:49:22 -0500

    Fixing ispell program to aspell.

--- internals/osx.el
+++ internals/osx.el
@@ -2,4 +2,4 @@
   (setq ns-command-modifier 'meta)
   (setq interprogram-paste-function 'x-selection-value)
   (setq browse-url-browser-function 'browse-url-default-macosx-browser)
-  (setq ack-executable "ack"))
+  (setq ispell-program-name "aspell"))
