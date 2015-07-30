fb2128e9b77795b6c2c7a53327c2ce70eb652c11
jwinder <jsph.winder@gmail.com>
2015-07-29 21:23:50 -0500

    Fixing package archives

--- deps/init.el
+++ deps/init.el
@@ -1,8 +1,8 @@
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpha/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
 
