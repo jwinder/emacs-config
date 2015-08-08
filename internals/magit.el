3f81ba80c3962c6c799200d12a9aad28d8f8f691
jwinder <jsph.winder@gmail.com>
2015-08-07 20:20:28 -0500

    Adding github commands to magit dispatch.

--- internals/magit.el
+++ internals/magit.el
@@ -8,24 +8,43 @@
   (interactive)
   (magit-run-git "undo"))
 
-(defun magit-x-github-browse ()
+(defun magit-github-browse ()
   (interactive)
   (shell-command "hub browse"))
 
-(defun magit-x-github-compare ()
+(defun magit-github-issues ()
   (interactive)
-  (shell-command "hub compare"))
+  (shell-command "hub browse -- issues"))
+
+(defun magit-github-pulls ()
+  (interactive)
+  (shell-command "hub browse -- pulls"))
+
+(defun magit-github-compare ()
+  (interactive)
+  (shell-command "hub browse -- compare"))
+
+(defun magit-github-pull-request ()
+  (interactive)
+  (magit-run-git-with-editor "pull-request"))
 
 (magit-define-popup magit-git-extras-popup
   "Popup console for git-extras commands."
   'magit-commands
   :man-page "git-extras"
-  :switches nil
-  :actions '((?L "Blaming" magit-blame-popup)
-             (?u "Undoing" magit-x-undo)
-             (?b "Github Browsing" magit-x-github-browse)
-             (?c "Github Comparing" magit-x-github-compare))
-  :default-arguments nil)
+  :actions '((?g "Github" magit-github-popup)
+             (?b "Blaming" magit-blame-popup)
+             (?u "Undo commit" magit-x-undo)))
+
+(magit-define-popup magit-github-popup
+  "Popup console for github hub commands."
+  'magit-commands
+  :man-page "hub"
+  :actions '((?b "Browse" magit-github-browse)
+             (?i "Issues" magit-github-issues)
+             (?p "Pulls" magit-github-pulls)
+             (?c "Compare" magit-github-compare)
+             (?P "Pull Request" magit-github-pull-request)))
 
 (magit-define-popup-action 'magit-dispatch-popup ?g "Status" 'magit-status)
 (magit-define-popup-action 'magit-dispatch-popup ?x "Extras" 'magit-git-extras-popup)
