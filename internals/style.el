f81976956c3f58e826c187795b9b8d9bf40330b6
jwinder <jsph.winder@gmail.com>
2015-08-12 20:19:32 -0500

    Showing different symbol in mode line if git is dirty.

--- internals/style.el
+++ internals/style.el
@@ -1,3 +1,5 @@
+(require 'magit) ;; for magit-anything-modified-p
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -10,7 +12,11 @@
 (set-face-attribute 'mode-line-highlight nil :foreground "#7db5d6")
 (set-face-attribute 'header-line nil :background "#005858" :foreground "white")
 
-(setq-default mode-line-format '(" ✔ " mode-line-buffer-identification " " mode-line-misc-info))
+(setq-default mode-line-format '(" " (vc-mode vc-mode) " " mode-line-buffer-identification " " mode-line-misc-info))
+
+(advice-add 'vc-mode-line :after #'(lambda (file &optional backend)
+                                     (when (stringp vc-mode)
+                                       (setq vc-mode (if (magit-anything-modified-p) "✘" "✔")))))
 
 (custom-set-faces '(eshell-prompt ((nil (:foreground "#d68f7d")))))
 
