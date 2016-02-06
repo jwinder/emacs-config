3f257321e7c1f089d38e2fb7397dbc73e99e55f5
jwinder <jsph.winder@gmail.com>
2016-02-06 10:52:54 -0600

    Using up-to-date version of org

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(unless (file-exists-p package-user-dir) (package-refresh-contents))
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+
+(require 'use-package)
+(use-package org-plus-contrib :ensure t)
+
+(setq package-enable-at-startup nil)
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
