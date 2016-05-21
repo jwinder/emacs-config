401f49cf88ee1eec85421e58e96152b69ded0555
jwinder <jsph.winder@gmail.com>
2016-05-21 07:29:47 -0500

    Cleanup use-package syntax to always set ensure to true

--- init.el
+++ init.el
@@ -5,9 +5,10 @@
 
 (unless (file-exists-p package-user-dir) (package-refresh-contents))
 (unless (package-installed-p 'use-package) (package-install 'use-package))
+(setq use-package-always-ensure t)
 
 (require 'use-package)
-(use-package org-plus-contrib :ensure t)
+(use-package org-plus-contrib)
 
 (setq package-enable-at-startup nil)
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
