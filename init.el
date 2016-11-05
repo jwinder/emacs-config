cddbbc687339073e64c9a8201118c72d5396a24a
jwinder <jsph.winder@gmail.com>
2016-11-05 10:55:46 -0500

    ensime

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (package-initialize)
 
 (unless (file-exists-p package-user-dir) (package-refresh-contents))
