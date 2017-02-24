cc75975432aa24cc6c671a10d28f7511246c58cc
jwinder <jsph.winder@gmail.com>
2017-02-23 18:26:11 -0600

    Fixing org-plus-contrib loading in init.el

--- init.el
+++ init.el
@@ -2,15 +2,13 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
 (package-initialize)
 
 (unless (file-exists-p package-user-dir) (package-refresh-contents))
 (unless (package-installed-p 'use-package) (package-install 'use-package))
-(setq use-package-always-ensure t)
 
 (require 'use-package)
-(use-package org-plus-contrib)
+(setq use-package-always-ensure t)
+(use-package org :ensure org-plus-contrib)
 
-(setq package-enable-at-startup nil)
-(org-babel-load-file (concat user-emacs-directory "emacs.org"))
+(org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
