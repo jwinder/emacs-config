4e51cc55c1a321470f2a3dc1f45fe442358fad62
jwinder <jsph.winder@gmail.com>
2022-01-18 05:18:42 -0600

    switch org from orgmode.org to gnu elpa. kill org-plus-contrib.

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 (unless (file-exists-p package-user-dir) (package-refresh-contents))
@@ -9,6 +8,6 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-(use-package org :ensure org-plus-contrib :pin org)
+(use-package org)
 
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
