9b67cc82c068d5b5e1d3fd244b9b1d9e8e4de786
jwinder <jsph.winder@gmail.com>
2023-08-01 07:03:22 -0500

    cleanup init.el file

--- init.el
+++ init.el
@@ -1,8 +1,2 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(unless (file-exists-p package-user-dir) (package-refresh-contents))
-
 (require 'org)
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
