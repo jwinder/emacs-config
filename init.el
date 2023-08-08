a528b73adfa254664d6c9e9709703270c84b95bd
jwinder <jsph.winder@gmail.com>
2023-08-08 04:16:59 -0500

    Revert emacs 29 changes

--- init.el
+++ init.el
@@ -1,2 +1,13 @@
-(require 'org)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless (file-exists-p package-user-dir) (package-refresh-contents))
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package org)
+
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
