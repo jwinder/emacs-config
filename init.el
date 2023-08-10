ab00214d2e577552a85b61d9a3313e027169a6a8
jwinder <jsph.winder@gmail.com>
2023-08-10 07:57:39 -0500

    garbage-collect on focus-out-hook

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq gc-cons-threshold 100000000)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
